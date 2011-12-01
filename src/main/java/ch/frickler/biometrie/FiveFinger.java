3f6a54af5d6955f04d770e4b20457c10e95151d6
John Ashton <chris.feuz@gmail.com>
2011-12-01 15:20:39 +0100

    Added resource and FiveFingerFileReader

---
+++ src/main/java/ch/frickler/biometrie/FiveFinger.java
@@ -0,0 +1,14 @@
+package ch.frickler.biometrie;
+
+
+public class FiveFinger {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		
+		new FingerFileReader();
+
+	}
+}
